3e0f2649a8d859d521f137a3648f70bc73c3b4b3
Giovanni Tempobono <giovanni.tempobono@alura.com.br>
2019-08-14 18:49:28 -0300

    aula1

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+require 'config.php';
+
+include 'Artigo.php';
+$artigo = new Artigo($mysql);
+$artigos = $artigo->exibirTodos();
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Meu Blog</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div id="container">
+        <h1>Meu Blog</h1>
+        <?php foreach ($artigos as $artigo) : ?>
+        <h2>
+            <a href="artigo.php?id=<?php echo $artigo['id']; ?>">
+                <?php echo $artigo['titulo']; ?>
+            </a>
+        </h2>
+        <p>
+            <?php echo $artigo['conteudo']; ?>
+        </p>
+        <?php endforeach; ?>
+    </div>
+</body>
+
+</html>
